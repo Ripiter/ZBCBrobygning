22b1de30261373df5337b44de3c291097ebf28ae
Tobias Prang <tobi8268@zbc.dk>
2020-10-27 15:28:57 +0100

    QuestManager Update
    Quest8 -> Quest13

---
+++ Assets/OurOwnStuff/Scripts/Quests/Quest12.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest12 : Quest
+{
+    public Quest12(int _id, int _questSize) : base(_id, _questSize)
+    {
+    }
+
+    public override void CheckForVictory()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateQuest(string objectivename)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateText()
+    {
+        throw new System.NotImplementedException();
+    }
+}
