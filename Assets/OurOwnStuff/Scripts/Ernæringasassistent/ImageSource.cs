9fb972dfa65cd56ea078df402d4e33e6a8d29268
NansyT <nanna.tronbjerg@gmail.com>
2020-10-28 15:12:11 +0100

    Ernæringsassistent

---
+++ Assets/OurOwnStuff/Scripts/Ernæringasassistent/ImageSource.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityScript.Steps;
+
+public class ImageSource : MonoBehaviour
+{
+    public List<ImageEneringsassistent> imglist = new List<ImageEneringsassistent>();
+    public RawImage rawImg;
+    public static ImageSource instance;
+    public int currentNum;
+    void Start()
+    {
+        //currentNum = GetRandomPlaceFromList();
+        //rawImg.texture = imglist[currentNum].img;
+    }
+    private void Awake()
+    {
+        //Check if gamemanager already exists
+        if (instance == null)
+
+            //if not, set gamemanager to this
+            instance = this;
+
+        //If gamemanager already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public ImageEneringsassistent GetRandomPlaceFromList()
+    {
+        if (imglist.Count == 0)
+            print("No more");
+
+        return imglist[Random.Range(0, imglist.Count)];
+    }
+}
+
+[System.Serializable]
+public struct ImageEneringsassistent
+{
+    public Texture img;
+    public string name;
+}
---
+++ Assets/OurOwnStuff/Scripts/Ernæringasassistent/Logic.cs
@@ -0,0 +1,57 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Logic : MonoBehaviour
+{
+    int correctAnswers = 1;
+    public TextMeshProUGUI text;
+    int newNum;
+    int tempNum;
+    int count = 1;
+
+    ImageEneringsassistent currentImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "Træk det rigtige navn på pladen over billedet";
+        currentImage = ImageSource.instance.GetRandomPlaceFromList();
+        ImageSource.instance.rawImg.texture = currentImage.img;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ImageSource.instance.imglist.Count == 0)
+        {
+            text.text = $"Du fik {correctAnswers} ud af 10";
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == ImageSource.instance.rawImg.texture.name)
+        {
+
+            Destroy(col.gameObject);
+
+            // Remove old image
+            ImageSource.instance.imglist.Remove(currentImage);
+            if (ImageSource.instance.imglist.Count > 0)
+            {
+                // Get new image
+                currentImage = ImageSource.instance.GetRandomPlaceFromList();
+                ImageSource.instance.rawImg.texture = currentImage.img;
+                text.text = "Korrekt svar!";
+                correctAnswers++;
+            }
+        }
+        else if (col.gameObject.tag != ImageSource.instance.rawImg.texture.name)
+        {
+            text.text = "Forkert svar!";
+            correctAnswers--;
+        }
+    }
+}
