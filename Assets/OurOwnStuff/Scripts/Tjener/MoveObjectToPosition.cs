2266b2c5da5b7f178a7a66d5a3add76583d04a3a
NansyT <nanna.tronbjerg@gmail.com>
2020-10-29 15:52:45 +0100

    Updates 29-10-2020

---
+++ Assets/OurOwnStuff/Scripts/Tjener/MoveObjectToPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectToPosition : MonoBehaviour
+{
+    public string objectName;
+    public GameObject prefab;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<NameOfObject>() != null)
+        {
+            if (other.gameObject.GetComponent<NameOfObject>().objectName == objectName)
+            {
+                GameObject spawned = Instantiate(prefab, transform.position, Quaternion.identity);
+                spawned.transform.rotation = transform.rotation;
+                Destroy(other.gameObject);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
