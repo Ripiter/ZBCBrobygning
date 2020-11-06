cfae5b4ee4fe8c6588d34729f97452f786f07c08
NansyT <nanna.tronbjerg@gmail.com>
2020-11-06 08:57:42 +0100

    ww

---
+++ Assets/OurOwnStuff/Scripts/PersonVognsMekaniker/EngineInPlace.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class EngineInPlace : MonoBehaviour
+{
+    public string NameOfObject;
+    public GameObject Prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<NameOfObject>() != null)
+        {
+            print(other.gameObject.GetComponent<NameOfObject>().objectName);
+            if (other.gameObject.GetComponent<NameOfObject>().objectName == NameOfObject)
+            {
+                GameObject spawned =  Instantiate(Prefab, transform);
+                spawned.transform.localScale = other.transform.localScale;
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
